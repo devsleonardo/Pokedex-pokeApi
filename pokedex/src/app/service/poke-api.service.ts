e2fcbf815727cb154bc47daa541af1b948627839
devsleonardo <usr.leonardo@gmail.com>
2022-01-08 09:44:30 -0300

    PlayService

---
+++ pokedex/src/app/service/poke-api.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+//Observable
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokeApiService {
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=100';
+  constructor(private http: HttpClient) {}
+
+  get apiListAllPokemons(): Observable<any> {
+    return this.http.get<any>(this.url).pipe(
+      tap((res) => res),
+      tap((res) =>
+        res.results.map((resPokemon: any) => {
+          resPokemon.status = res;
+        })
+      )
+    );
+  }
+}
